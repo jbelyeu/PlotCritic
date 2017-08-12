35116436b04c0b4825879ea5b0ae6d129ac16773
jrbelyeu <jrbelyeu@gmail.com>
2017-08-11 20:29:40 -0600

    functional now

--- sveee.js
+++ sveee.js
@@ -1,7 +1,6 @@
 var app = angular.module("svApp", []);
 
 app.directive('keypressEvents',
-
 function ($document, $rootScope) {
     return {
         restrict: 'A',
@@ -14,66 +13,97 @@ function ($document, $rootScope) {
 });
 
 app.controller("svCtrl", function($scope, $rootScope, $timeout) {
-	$scope.images = ['http://sveee.s3-website-us-east-1.amazonaws.com/na12878_cnmops/10_114112001-114117000.png'];
+	$scope.images = [];
 	$scope.currentImageIdx = 0;
-	$scope.imgsURL = "http://sv.s3-website-us-east-1.amazonaws.com/na12878_cnmops/";
+	$scope.imgsURL = "http://sveee.s3-website-us-east-1.amazonaws.com/";
 	$scope.currentImage = '';
 	$scope.goodButton = ["good_button"];
 	$scope.badButton = ["bad_button"];
-	$scope.viewerSelected = '';	
+	$scope.unclearButton = ["unclear_button"];
+	$scope.variantImgSelected = '';	
 	$scope.reachedEnd = false;
 	$scope.reachedStart = false;
+	$scope.email = '';
+	$scope.hide = false;
+	var accessKey = 'AKIAI63BOTTCFQX556OQ';
+	var secretAccessKey = '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';
+	var dynamoEndpoint = 'https://dynamodb.us-east-1.amazonaws.com';
+	var region = 'us-east-1';
+	var dynamoTable = 'sveee';
+	var bucketName = 'sveee';
+	var imgTypes = ['.png', ".jpg", ".jpeg"];
 
     $rootScope.$on('keypress', function (evt, obj, key) {
         $scope.$apply(function () {
-            if (key == 'g' || key == 'G') {
-            	$scope.goodVariant();
-            }
-            else if (key == 'b' || key == 'B') {
-            	$scope.badVariant();
-            }
+        	if ($scope.currentImage != "") {
+        		if (key == 'g' || key == 'G') {
+	            	$scope.goodVariant();
+	            }
+	            else if (key == 'b' || key == 'B') {
+	            	$scope.badVariant();
+	            }
+        	}            
         });
     })
 
-	// this function will send to the server and log whether this is a good or bad variant
 	var sendScore = function(flag) {
-		console.log("unimplemented");
-		console.log($scope.currentImage);
-		console.log(flag);
+		AWS.config.update({
+			accessKeyId: accessKey, 
+			secretAccessKey: secretAccessKey,
+			endpoint: dynamoEndpoint,
+			region: region
+		});
+
+		var docClient = new AWS.DynamoDB.DocumentClient();
+		var now = new Date();
+		var params = {
+		    TableName:dynamoTable,
+		    Item:{
+		    	'identifier': $scope.email + "_" + $scope.currentImage,
+		        "email": $scope.email,
+		        'image': $scope.currentImage,
+		        'bucket': $scope.imgsURL,
+		        'timestamp': now.toString(),
+		        'score': flag
+		    }
+		};
+		docClient.put(params, function(err, data) {
+		    if (err) {
+		        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+		    }
+		});
 	};
 
 	var init = function () {
-		var creds = new AWS.Credentials({
-		  accessKeyId: 'AKIAJ3LYC2HDTNJIOYWA', secretAccessKey: '1CIrjsfzwU94ODsXMmn0SEc79'
+		var config = new AWS.Config({accessKeyId: accessKey, secretAccessKey: secretAccessKey, region: region});
+
+		var bucket = new AWS.S3(config);
+		bucket.listObjects({Bucket: bucketName}, function (err, data) {
+		    if (err) {
+		      console.log(err);
+		    } else {
+		    	for (var i = 0; i < data.Contents.length; i++) {
+		    		var resourceName = data.Contents[i]['Key'];
+		    		if (resourceName.substring(resourceName.length-4) == imgTypes[0] || 
+		    			resourceName.substring(resourceName.length-4) == imgTypes[1] || 
+		    			resourceName.substring(resourceName.length-5) == imgTypes[2]) {
+		    			resourceName = $scope.imgsURL + resourceName;
+
+		    			$scope.images.push(resourceName);
+		    		}
+		    	}
+			    shuffleArray($scope.images);
+		    }
+    		$scope.currentImage = $scope.images[$scope.currentImageIdx];
 		});
-// http://sveee.s3-website-us-east-1.amazonaws.com/na12878_cnmops/files.txt
-
-		var bucket = new AWS.S3(creds);
-		var params = {
-			Key: "na12878_cnmops/files.txt",
-			Bucket: 'sveee'};
-
-		bucket.getObject(params, function(err, data) {
-			if (err) {
-				console.log(err);
-			}
-			else {
-				console.log(data);	
-			}
-		});
-
-
-		// bucket.listObjects(params, function (err, data) {
-		//     if (err) {
-		//       console.log(err);
-		//     } else {
-		//       console.log(data);
-		//     }
-		// });
-
-
-		$scope.currentImage = $scope.images[$scope.currentImageIdx];
 	};
+
+	var shuffleArray = function (arr) {
+		for (let i = $scope.images.length; i; i--) {
+	        let j = Math.floor(Math.random() * i);
+	        [$scope.images[i - 1], $scope.images[j]] = [$scope.images[j], $scope.images[i - 1]];
+	    }
+	}
 
 	var resetCurrent = function (change) {
 		$scope.currentImageIdx += change;
@@ -83,32 +113,41 @@ app.controller("svCtrl", function($scope, $rootScope, $timeout) {
 	//scope functions
 	$scope.goodVariant = function () {
 		$scope.goodButton.push("good_button_dark");
-		$scope.viewerSelected = "viewerGood";
+		$scope.variantImgSelected = "variantImgGood";
 		sendScore(true);
 		$timeout(function() { 
 			$scope.goodButton.pop();
-			$scope.viewerSelected = "";
-		}, 500);
+			$scope.variantImgSelected = "";
+		}, 50);
+		$scope.next();
+	};
+
+	$scope.unclearVariant = function () {
+		$scope.unclearButton.push("unclear_button_dark");
+		$scope.variantImgSelected = "variantImgUnclear";
+		sendScore('unclear');
+		$timeout(function() { 
+			$scope.unclearButton.pop();
+			$scope.variantImgSelected = "";
+		}, 50);
 		$scope.next();
 	};
 
 	$scope.badVariant = function () {
 		$scope.badButton.push("bad_button_dark");
-		$scope.viewerSelected = "viewerBad";
+		$scope.variantImgSelected = "variantImgBad";
 		sendScore(false);
 		$timeout(function() { 
 			$scope.badButton.pop();
-			$scope.viewerSelected = "";
-		}, 500);
+			$scope.variantImgSelected = "";
+		}, 50);
 		$scope.next();
 	};
 
 	$scope.previous = function () {
-		console.log("previous");
 		$scope.reachedEnd = false;
 		$scope.reachedStart = false;
 
-		//if there are more images, should call resetCurrent to change current image to next image and update index
 		if ($scope.currentImageIdx -1 > 0) {
 			resetCurrent(1);
 		}
@@ -118,11 +157,9 @@ app.controller("svCtrl", function($scope, $rootScope, $timeout) {
 	};
 
 	$scope.next = function () {
-		console.log("next");
 		$scope.reachedEnd = false;
 		$scope.reachedStart = false;
 
-		//if there are more images, should call resetCurrent to change current image to next image and update index
 		if ($scope.images.length > $scope.currentImageIdx +1) {
 			resetCurrent(1);
 		}
@@ -131,6 +168,27 @@ app.controller("svCtrl", function($scope, $rootScope, $timeout) {
 		}
 	};
 
+	$scope.beginning = function () {
+		resetCurrent(-$scope.currentImageIdx);
+		$scope.reachedStart = true;
+
+		if ($scope.currentImageIdx +1 !== $scope.images.length) {
+			$scope.reachedEnd = false;
+		}
+	};
+	$scope.ending = function () {
+		resetCurrent($scope.images.length-$scope.currentImageIdx);
+		$scope.reachedEnd = true;
+
+		if ($scope.currentImageIdx !== 0) {
+			$scope.reachedStart = false;
+		}
+	};
+
+	$scope.submit = function() {
+		if ($scope.email != '' && $scope.currentImage != '') {
+			$scope.hide = true;
+	    }
+	};
 	init();
 });
-
