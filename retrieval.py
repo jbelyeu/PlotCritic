302d450639ae59a34224d7c1adc920389e47a677
jrbelyeu <jrbelyeu@gmail.com>
2017-08-17 14:43:13 -0600

    reformatted retrieval for ease of use

--- retrieval.py
+++ retrieval.py
@@ -9,11 +9,13 @@ with open("config.json",'r') as config_file:
 dynamodb = boto3.resource('dynamodb', region_name=config_data['region'], endpoint_url=config_data['dynamoEndpoint'])
 sveee_table = dynamodb.Table(config_data['dynamoTable'])
 try:
-#    response = sveee_table.query(KeyConditionExpression=Key('identifier').scan())
     response = sveee_table.scan()
 except ClientError as e:
     print (e.response['Error']['Message'])
 else:
+    header_fields = ["BUCKET","EMAIL","IMAGE", "SCORE", "TIMESTAMP", "PROJECT", "IDENTIFIER"]
+    print ("#" + "\t".join(header_fields))
     for entry in response['Items']:
-        print("\nEntry")
-        pprint (entry)
+        if not entry['project']:
+            entry['project'] = "*"
+        print ("\t".join([entry['bucket'],entry['email'],entry['image'],str(entry['score']),str(entry['timestamp']), entry['project'], entry['identifier']]))
